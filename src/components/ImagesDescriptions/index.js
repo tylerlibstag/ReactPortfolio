6eaa6adfa1daf1b91bbcd80266e9dcc71ab7e598
TYTY <tylerlibstag@gmail.com>
2021-10-14 11:06:21 -0400

    fixed buttons

--- src/components/ImagesDescriptions/index.js
+++ src/components/ImagesDescriptions/index.js
@@ -23,8 +23,8 @@ function ImagesDescriptions() {
                   <p class="description">Razer is known for building the worlds largest gamer-focused ecosystem of hardware services and software.
                      Their iconic logo is recognized by gaming and esports communites around the globe. Aside for my love for videogames, I am happy to have contributed to the visual layout of one of their most
                      utilized aspects of their website which is the the Razer RMA Process. </p>
-                  <button class="button">Creative Process</button>
-                  <button class="button2">Website</button>
+                  <button class="button"><a href="/razer">Creative Process</a></button>
+                  <button class="button2"><a href="https://tst2.dev.mysupport.razer.com/app" target="_blank">Website</a></button>
 
 
                </div>
@@ -35,8 +35,8 @@ function ImagesDescriptions() {
                <div>
                   <p class="description">James Avery is an American made jewelry company Located in Texas Hill County. They offer finely crafted jewelry for both men and women.
                      Aside from rubies and sapphires, the real gem was being able to see this project through while also learning how to use the Policy Modeler Program. Similar to Razer, I played a role in the visual layout of James Avery's fundraising support and Corporate grants process.  </p>
-                  <button class="button">Creative Process</button>
-                  <button class="button2">Website</button>
+                     <button class="button"><a href="/JamesAvery">Creative Process</a></button>
+                  <button class="button2"><a href="https://jamesavery.custhelp.com/app/home" target="_blank">Website</a></button>
                </div>
             </div>
             <div className="project-container-images">
@@ -44,8 +44,8 @@ function ImagesDescriptions() {
                <div>
                   <p class="description">First Savings bank has been around since 1913 and started in south Dakota. The family owned business has expanded to locations such as, New Mexico Nevada, Nebraska, Texas and Arizona.
                      Unironically, First Savings bank was the first company project I had worked on. My contribution was setting up part of the support page powered by Oracle </p>
-                  <button class="button">Creative Process</button>
-                  <button class="button2">Website</button>
+                     <button class="button"><a href="/FirstSavings">Creative Process</a></button>
+                  <button class="button2"><a href="https://opn-infolexa1.rightnowdemo.com/" target="_blank">Website</a></button>
                  
                </div>
 
@@ -55,8 +55,8 @@ function ImagesDescriptions() {
                <img src={Boro} className="razer" />
                <div>
                   <p class="description">(Personal Project)A project created for users to apartment hunt from the comfort of their homes. (Update: our database and api keys are down but our visuals are still present! Click to explore) </p>
-                  <button class="button">Creative Process</button>
-                  <button class="button2">Website</button>
+                  <button class="button"><a href="/Boroughd">Creative Process</a></button>
+                  <button class="button2"><a href="https://aptreact.web.app/" target="_blank">Website</a></button>
                </div>
 
             </div>
@@ -65,9 +65,9 @@ function ImagesDescriptions() {
                <img src={Cinder} className="razer" />
                <div>
                   <p class="description">(Personal Project) This project was created with the intent of helping both new and veteran gamers find fitting games based on the users criteria. Click to find the right game for you!  </p>
-                  <button class="button">Creative Process</button>
-                  <button class="button2">Website</button>
-                  <Buttons label="button" />
+                  <button class="button"><a href="/Cinder">Creative Process</a></button>
+                  <button class="button2"><a href="https://project2-cinder.herokuapp.com/" target="_blank">Website</a></button>
+                
                </div>
 
             </div>
